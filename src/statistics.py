985305ffa87d0ecf04e899ce9231d797603e7d74
Chris Tang <chris.l.tang@gmail.com>
2015-05-11 01:56:55 -0400

    Gather summary statistics

---
+++ src/statistics.py
@@ -0,0 +1,71 @@
+import cPickle as pickle
+import numpy as np
+import scipy.stats as spstats
+import os
+
+from models.movies import Movies
+from models.ratings import Ratings
+from models.users import Users
+
+
+class SummaryStatistics(object):
+
+    def __init__(self, movies, ratings, users):
+        self.movies = movies
+        self.ratings = ratings
+        self.users = users
+
+        self.movie_stats = self.summary_movies()
+        self.user_stats = self.summary_users()
+
+    def summary_movies(self):
+        summary = {}
+
+        for m in self.movies.movies:
+            movie_ratings = self.ratings.for_movie(m.ID)
+            summary[m.ID] = SummaryStatistics.summary_stats(movie_ratings)
+
+        return summary
+
+    def summary_users(self):
+        summary = {}
+
+        for u in self.users.users:
+            movie_ratings = self.ratings.for_user(u.ID)
+            summary[u.ID] = SummaryStatistics.summary_stats(movie_ratings)
+
+        return summary
+
+    @staticmethod
+    def summary_stats(movie_ratings):
+        count = len(movie_ratings)
+        if count > 0:
+            amean = np.mean([r.rating for r in movie_ratings])
+            hmean = spstats.hmean([r.rating for r in movie_ratings])
+            var = np.var([r.rating for r in movie_ratings])
+        else:
+            amean = np.NaN
+            hmean = np.NaN
+            var = np.NaN
+        return count, amean, hmean, var
+
+
+def get_workspace(data_dir):
+
+    cwd = data_dir
+    pickle_fn = os.path.join(cwd, 'stats.pkl')
+
+    if not os.path.isfile(pickle_fn):
+        movies = Movies.parse_stream(open(os.path.join(cwd, 'movies.dat')))
+        ratings = Ratings.parse_stream(open(os.path.join(cwd, 'ratings.dat')))
+        users = Users.parse_stream(open(os.path.join(cwd, 'users.dat')))
+
+        stats = SummaryStatistics(movies, ratings, users)
+        pickle.dump(stats, open(pickle_fn, 'wb'))
+    else:
+        stats = pickle.load(open(pickle_fn))
+
+    return stats
+
+if __name__ == '__main__':
+    get_workspace('dat/ml-1m')
