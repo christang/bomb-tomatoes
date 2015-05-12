b44fe0675e16d0ff9dd53ded8a11b7a93faf18bf
Chris Tang <chris.l.tang@gmail.com>
2015-05-12 08:07:05 -0400

    Track an ID for ratings by list index

--- src/models/ratings.py
+++ src/models/ratings.py
@@ -13,22 +13,23 @@ class Ratings(object):
 
     @staticmethod
     def parse_stream(stream):
-        ratings = [Rating.parse_entry(line) for line in stream]
+        ratings = [Rating.parse_entry(id_, line) for id_, line in enumerate(stream)]
         return Ratings(ratings)
 
 
 class Rating(object):
 
-    def __init__(self, u_id, m_id, rating, timestamp):
+    def __init__(self, id_, u_id, m_id, rating, timestamp):
+        self.ID = id_
         self.u_id = u_id
         self.m_id = m_id
         self.rating = rating
         self.timestamp = timestamp
 
     @staticmethod
-    def parse_entry(line):
+    def parse_entry(id_, line):
         items = line.strip().split('::')
-        return Rating(int(items[0]), int(items[1]), int(items[2]), items[3])
+        return Rating(id_, int(items[0]), int(items[1]), int(items[2]), items[3])
 
     def __repr__(self):
         return '::'.join([str(self.u_id), str(self.m_id), str(self.rating), self.timestamp])
