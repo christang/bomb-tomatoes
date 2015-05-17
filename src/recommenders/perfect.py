a6a5548f47a39579ad09ffe57721c47de6e63fdd
Chris Tang <chris.l.tang@gmail.com>
2015-05-17 15:48:05 -0400

    Add perfect recommender; returns ground truth

---
+++ src/recommenders/perfect.py
@@ -0,0 +1,17 @@
+from base import *
+
+
+class PerfectRecommender(BaseRecommender):
+
+    def rank_movies(self, uid, mid_subset=None):
+        ratings = self.ratings.for_user(uid)
+        ratings = [r for r in ratings if r.m_id in mid_subset]
+        ratings.sort()
+
+        last_rank = 0
+        last_rating = None
+        for r in ratings:
+            if r.rating != last_rating:
+                last_rank += 1
+                last_rating = r.rating
+            yield Rank(user=r.u_id, movie=r.m_id, rank=last_rank, score=r.rating)
