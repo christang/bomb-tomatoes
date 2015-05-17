43852bbfcdb764c49ad4efbc3fbfff3b63b205f1
Chris Tang <chris.l.tang@gmail.com>
2015-05-17 13:44:57 -0400

    Remove pilot code from Folds

--- src/folds.py
+++ src/folds.py
@@ -1,6 +1,5 @@
-from workspace import *
-
 import random
+
 from sklearn import cross_validation
 
 
@@ -11,25 +10,6 @@ class Folds(object):
         self.users = users
         self.folds = {u.ID: expand(cross_validation.KFold(u.count, n_folds=k, shuffle=True))
                       for u in users.users if u.ID <= limit_users}
-
-    def show(self, first_n=25):
-        train = []
-        test = []
-
-        for k in xrange(self.k):
-            train.append([])
-            test.append([])
-
-        for u_id, kf in self.folds.items():
-            k = 0
-            for train_index, test_index in kf:
-                train[k].extend([str(self.users[u_id].r_ids[i]) for i in train_index])
-                test[k].extend([str(self.users[u_id].r_ids[i]) for i in test_index])
-                k += 1
-
-        for k in xrange(self.k):
-            print "TRAIN %d %s" % (k, ','.join(train[k][:first_n]))
-            print "TEST %d %s" % (k, ','.join(test[k][:first_n]))
 
     def train_on(self, k, uid_subset=None):
         uid_subset = uid_subset or self.folds.keys()
@@ -53,9 +33,3 @@ class Folds(object):
 
 def expand(kf):
     return [(train, test) for train, test in kf]
-
-
-if __name__ == '__main__':
-    w = get_workspace('dat/ml-1m')
-    f = Folds(w.users, 5)
-    f.show(5)
