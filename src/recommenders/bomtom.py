43f99ac262edf9337d4254562c9dd420f0626cad
Chris Tang <chris.l.tang@gmail.com>
2015-05-19 08:34:43 -0400

    Stub for our recommender of interest

---
+++ src/recommenders/bomtom.py
@@ -0,0 +1,10 @@
+from base import BaseRecommender, Rank
+
+
+class BomTomRecommender(BaseRecommender):
+
+    def rank_movies(self, uid, mid_subset=None):
+        pass
+
+    def train(self, training_set_gen):
+        pass
