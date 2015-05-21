0d815e35d0f604892989b86075d6c1386a6d6a30
Chris Tang <chris.l.tang@gmail.com>
2015-05-21 07:48:22 -0400

    Create the BomTom recommender
    Solves the Normal Equation to get coefficients that minimize the L2
    normal of:
    
      Ax(u)-b(u)
    
    where:
    
      A    is the movie-tag feature matrix
      b(u) is the movie-rating vector for a given user
      x(u) are the coefficients of interest for the user
    
    interesting todos:
    
      add the movie.decade() as a feature to A (easiest)
      add demographics preferences as a feature to A
        x(u) for such features can be interpretted as how closely u
        emulates the mean preference for the movie in that demographic
      add geographic preferences as a feature to A
        given zip code we can find mean preference for the movie by
        different geographic regions (do a lookup on geo by zip)
      determine better coefficients for tags of a movie, not simply
        binary (e.g. a movie might be better scored as 0.4 Horror
        but 0.8 Comedy)
      improve feature selection; ensure orthogonality
        feature tags may not be linearly independent, (e.g. a movie
        that is tagged Animation might be correlated with Children's)
        so we may want to do things like factor analysis, basis
        transformation
    
    so much more fun stuff to do

--- src/recommenders/bomtom.py
+++ src/recommenders/bomtom.py
@@ -1,10 +1,70 @@
-from base import BaseRecommender, Rank
+from collections import defaultdict
+import numpy as np
+
+from base import BaseRecommender
+
+
+class UserProfile(object):
+
+    untagged = 0.001
+
+    def __init__(self, user, movies, ratings):
+        self.user = user
+        self.ratings = ratings
+        self.f_coeffs = UserProfile.build_coeffs(ratings, movies)
+        #print 'f_coeffs = ', self.f_coeffs
+
+    def rate(self, movie):
+        score = movie.hmean
+        for tag, coeff in self.f_coeffs.items():
+            a = 1 if tag in movie.tags else UserProfile.untagged
+            score += a * coeff
+        return score
+
+    @staticmethod
+    def build_coeffs(ratings, movies):
+        m_index = sorted(ratings.keys())
+        f_index = sorted(set(t for m_id in m_index for t in movies[m_id].tags))
+        A_shape = (len(m_index), len(f_index))
+        A = np.zeros(A_shape)
+        b_shape = (len(m_index), 1)
+        b = np.zeros(b_shape)
+        for i, m_id in enumerate(m_index):
+            for j, tag in enumerate(f_index):
+                # Take MovieLens tags at *mostly* face value :-)
+                A[i, j] = 1 if tag in movies[m_id].tags else UserProfile.untagged
+            b[i, 0] = ratings[m_id] - movies[m_id].hmean
+
+        if len(m_index) < 1000:
+            # We seek to minimize: L2||Ax-b||
+            # => (A'A)^-1 dot A' dot b
+            # => pinv(A) dot b
+            x = np.linalg.pinv(A).dot(b)
+        else:
+            # don't solve for now
+            raise Exception('too many movies')
+        return {f: x[i, 0] for i, f in enumerate(f_index)}
 
 
 class BomTomRecommender(BaseRecommender):
 
+    def __init__(self, users, movies, ratings):
+        super(BomTomRecommender, self).__init__(users, movies, ratings)
+        self.profiles = {}
+
     def rank_movies(self, uid, mid_subset=None):
-        pass
+        mid_subset = mid_subset or self.movies.movies_by_ID.keys()
+        ratings = [(mid, self.profiles[uid].rate(self.movies[mid])) for mid in mid_subset]
+        for rank in BaseRecommender.sort_and_yield(ratings, uid):
+            yield rank
 
     def train(self, training_set_gen):
-        pass
+        r_ids = list(training_set_gen)
+
+        users = defaultdict(dict)
+        for r_id in r_ids:
+            r = self.ratings.ratings[r_id]
+            users[r.u_id][r.m_id] = r.rating
+
+        for u_id, ratings in users.items():
+            self.profiles[u_id] = UserProfile(self.users[u_id], self.movies, ratings)
