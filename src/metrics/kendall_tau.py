b244455093d03b34f3ab4e2001c3e1bf9cfbc4f5
Chris Tang <chris.l.tang@gmail.com>
2015-05-22 06:26:45 -0400

    Combine K-T statistic into one per fold

--- src/metrics/kendall_tau.py
+++ src/metrics/kendall_tau.py
@@ -9,7 +9,7 @@ class KendallTau(collections.namedtuple('Tau', 'user tau p')):
     __slots__ = ()
 
     def __repr__(self):
-        return 'tau(u:%d)=%4.2f[%4.2e]' % (self.user, self.tau, self.p)
+        return 'tau(u:%s)=%4.2f[%4.2e]' % (str(self.user), self.tau, self.p)
 
 
 class KendallTauMetric(BaseMetric):
@@ -18,3 +18,13 @@ class KendallTauMetric(BaseMetric):
         y_true, y_pred = self.ordered_scores(key)
         tau, p_value = kendalltau(y_true, y_pred)
         return KendallTau(user=key, tau=tau, p=p_value)
+
+    def evaluate_all(self):
+        all_true = []
+        all_pred = []
+        for key in self.keys():
+            y_true, y_pred = self.ordered_scores(key)
+            all_true.extend(y_true)
+            all_pred.extend(y_pred)
+        tau, p_value = kendalltau(all_true, all_pred)
+        return [KendallTau(user='all', tau=tau, p=p_value)]
