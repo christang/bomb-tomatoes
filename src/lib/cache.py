fca413d7bd52773f7d703e0c33a30088d728c410
Chris Tang <chris.l.tang@gmail.com>
2015-05-30 13:12:02 -0400

    Add retry logic and simple 5-sec throttle

--- src/lib/cache.py
+++ src/lib/cache.py
@@ -1,17 +1,36 @@
 import cPickle
 import os
+import time
+
+
+class RateLimitException(Exception):
+
+    pass
 
 
 class Pickled(object):
+
+    delay = 5
 
     def __init__(self, cwd='dat/pkl'):
         self.cwd = cwd
 
     @staticmethod
-    def load_or_compute(pickle_fn, compute):
-        if os.path.isfile(pickle_fn):
-            result = cPickle.load(open(pickle_fn))
-        else:
-            result = compute()
+    def load_or_compute(pickle_fn, compute, retry=0, retry_none=True):
+        result = cPickle.load(open(pickle_fn)) if os.path.isfile(pickle_fn) else None
+
+        if result is None and retry_none:
+            result = Pickled.try_compute(compute, retry)
             cPickle.dump(result, open(pickle_fn, 'wb'))
+
         return result
+
+    @staticmethod
+    def try_compute(compute, retry):
+        while retry > 0:
+            try:
+                return compute()
+            except RateLimitException:
+                time.sleep(Pickled.delay)
+                retry -= 1
+        return None
