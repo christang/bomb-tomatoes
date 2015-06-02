bb7f670ebf8096c8a01e672baf9aced34e9d81d9
Chris Tang <chris.l.tang@gmail.com>
2015-06-02 09:08:39 -0400

    Compute distance between zip codes

--- src/models/zip_code.py
+++ src/models/zip_code.py
@@ -1,3 +1,4 @@
+from geopy.distance import vincenty
 import os
 import requests
 
@@ -32,9 +33,21 @@ class ZipCode(Pickled):
         pickle_fn = os.path.join(self.cwd, 'zip_codes', self.zip_code)
         return Pickled.load_or_compute(pickle_fn, compute, retry=1)
 
+    def miles_to(self, other):
+        return ZipCode.distance_miles(self, other)
+
     @staticmethod
     def maps_resource(zip_code, key=None):
         resource = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s' % zip_code
         if key:
             resource += '&key=%s' % key
         return resource
+
+    @staticmethod
+    def distance_miles(zip1, zip2):
+        if zip1.metadata and zip2.metadata:
+            loc1 = (zip1.metadata['geometry']['location'][coord] for coord in ['lat', 'lng'])
+            loc2 = (zip2.metadata['geometry']['location'][coord] for coord in ['lat', 'lng'])
+            return vincenty(loc1, loc2).miles
+        else:
+            return float('nan')
